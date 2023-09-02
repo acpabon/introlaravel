52af7a122f2348735c821bcf456c04d66ccfe3d2
Andres Pabon <ing.andresp02@gmail.com>
2023-09-02 15:33:19 -0500

    clase 20230902

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,4 +44,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: function($value){
+                return ucwords($value);
+            },
+            set: function($value){
+                return strtolower($value);
+            }
+        );
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Curso;
+use App\Models\User;
 
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UserSeeder::class);
-        $this->call(CursoSeeder::class);
+        // $this->call(UserSeeder::class);
+        // $this->call(CursoSeeder::class);
+
+        Curso::factory(50)->create();
+        User::factory(10)->create();
     }
 }
